646f522f09954390bf2f79c0d90e21837c995673
martin <tiin.-@hotmail.es>
2024-10-22 01:10:42 -0300

    First Commit

---
+++ sistema-examenes-back/src/main/java/com/sistema/examenes/entity/User.java
@@ -0,0 +1,25 @@
+package com.sistema.examenes.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastname;
+    private String email;
+    private String telephone;
+    private boolean isActive = true;
+    private String profile;
+
+
+}
